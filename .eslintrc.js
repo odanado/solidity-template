9ecd3089be09deee6aed6e890281c9904499cba4
odan <odan3240@gmail.com>
2020-03-21 14:00:38 +0900

    chore(lint): separate a config of eslint between js and ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,33 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
   ],
-  plugins: [
-    "@typescript-eslint"
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    sourceType: "module"
-  },
+  plugins: [],
+  parser: 'babel-eslint',
   rules: {
   },
   globals: {
     artifacts: true
-  }
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint"
+      ],
+      plugins: [
+        "@typescript-eslint"
+      ],
+
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module"
+      },
+      rules: {
+      }
+    }
+  ]
 }
