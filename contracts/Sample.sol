963a132918197ef4b9f952ebc8d26a6668c47f40
odan <odan3240@gmail.com>
2021-01-05 22:48:19 +0900

    add event

--- contracts/Sample.sol
+++ contracts/Sample.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.6.1;
 
 contract Sample {
   string public value;
+  event SetValue(string value);
 
 
   function setValue(string memory _value) public {
     value = _value;
+    emit SetValue(_value);
   }
 
   function getValue() public view returns (string memory) {
